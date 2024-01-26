e52388f441458e4176034a2b40a9ca2878fa7568
Felipe Carvalho <feliped3carvalho@gmail.com>
2024-01-26 14:05:43 -0300

    config actions test

--- test/app/interactor/actions/config_action_test.dart
+++ test/app/interactor/actions/config_action_test.dart
@@ -1,42 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:yuno/app/interactor/actions/config_action.dart';
 import 'package:yuno/app/interactor/atoms/config_atom.dart';
 import 'package:yuno/app/interactor/models/game_config.dart';
+import 'package:yuno/app/interactor/repositories/config_repository.dart';
+import 'package:yuno/injector.dart';
+
+class ConfigRepositoryMock extends Mock implements ConfigRepository {}
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  setUpAll(() {
-    SharedPreferences.setMockInitialValues({});
-  });
   group('ConfigAction |', () {
     final config = GameConfig(themeMode: ThemeMode.dark);
     test('Should save the configuration', () async {
-      await saveConfig(config);
-      final shared = await SharedPreferences.getInstance();
-      final savedConfig = shared.getString('config');
-      expect(savedConfig, equals(config.toJson()));
+      //Arrange
+      final repository = ConfigRepositoryMock();
+      when(() => repository.saveConfig(config))
+          .thenAnswer((_) => Future.value());
+      injector.replaceInstance<ConfigRepository>(repository);
+      //Act
+      final result = saveConfig(config);
+      //Assert
+      expect(gameConfigState.next(), completion(config));
+      expect(result, completes);
     });
 
-    test('Should fetchConfig if json is null', () async {
-      await fetchConfig();
-      final shared = await SharedPreferences.getInstance();
-      expect(shared.getString('config'), equals(config.toJson()));
+    test('Should fetchConfig', () async {
+      // Arrange
+      final repository = ConfigRepositoryMock();
+      when(() => repository.getConfig())
+          .thenAnswer((_) => Future.value(config));
+      injector.replaceInstance<ConfigRepository>(repository);
+      //Act
+      final result = fetchConfig();
+      //Assert
+      expect(gameConfigState.next(), completion(config));
+      expect(result, completes);
     });
 
-    test('Should not fetchConfig if json is not null', () async {
-      await fetchConfig();
-      final shared = await SharedPreferences.getInstance();
-      expect(shared.getString('-'), isA<void>());
-    });
-
-    test('Should register time', () async {
-      await registerHourListener();
-      expect(hoursState.value,
-          equals(DateFormat('HH:mm').format(DateTime(2024, 1, 25, 00, 00))));
+    test('Should Open Url', () {
+      //Arrange
+      final uri = Uri.https('localhost.com', '/home');
+      final repository = ConfigRepositoryMock();
+      when(() => repository.openUrl(uri)).thenAnswer((_) => Future.value());
+      injector.replaceInstance<ConfigRepository>(repository);
+      //Act
+      final result = openUrl(uri);
+      //Assert
+      expectLater(result, completes);
     });
   });
 }
