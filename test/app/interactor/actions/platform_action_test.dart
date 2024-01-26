643ab997527c4a1870253940b64db4fa65b18502
Felipe Carvalho <feliped3carvalho@gmail.com>
2024-01-26 02:27:12 -0300

    added platform action  test group

--- test/app/interactor/actions/platform_action_test.dart
+++ test/app/interactor/actions/platform_action_test.dart
@@ -1,97 +1,112 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:yuno/app/interactor/actions/platform_action.dart';
 import 'package:yuno/app/interactor/models/embeds/game.dart';
+import 'package:yuno/app/interactor/models/embeds/game_category.dart';
+import 'package:yuno/app/interactor/models/platform_model.dart';
 
 void main() {
-  test('Uri to path', () {
-    expect(
-      convertContentUriToFilePath(
-          'content://com.android.externalstorage.documents/tree/primary%3AEmulation%2Frooms%2Fsnes/document/primary%3AEmulation%2Frooms%2Fsnes'),
-      '/storage/emulated/0/Emulation/rooms/snes',
-    );
-  });
+  group('PlatformAction |', () {
+    test('Uri to path', () {
+      expect(
+        convertContentUriToFilePath(
+            'content://com.android.externalstorage.documents/tree/primary%3AEmulation%2Frooms%2Fsnes/document/primary%3AEmulation%2Frooms%2Fsnes'),
+        '/storage/emulated/0/Emulation/rooms/snes',
+      );
+    });
 
-  test('clean name', () {
-    final megaman = cleanName('Mega Man Zero 3.zip');
-    expect(megaman, 'Mega Man Zero 3');
-  });
+    test('clean name', () {
+      final megaman = cleanName('Mega Man Zero 3.zip');
+      expect(megaman, 'Mega Man Zero 3');
+    });
 
-  test('Sync games add', () {
-    final currentGames = [
-      Game(
-        name: 'game 1',
-        description: 'aaa',
-        image: '',
-        path: '/path1',
-      ),
-      Game(
-        name: 'game 2',
-        description: 'aaa',
-        image: '',
-        path: '/path2',
-      ),
-    ];
-    final folderGames = [
-      Game(
-        name: 'game 1',
-        description: '',
-        image: '',
-        path: '/path1',
-      ),
-      Game(
-        name: 'game 2',
-        description: '',
-        image: '',
-        path: '/path2',
-      ),
-      Game(
-        name: 'game 3',
-        description: '',
-        image: '',
-        path: '/path3',
-      ),
-    ];
+    test('Sync games add', () {
+      final currentGames = [
+        Game(
+          name: 'game 1',
+          description: 'aaa',
+          image: '',
+          path: '/path1',
+        ),
+        Game(
+          name: 'game 2',
+          description: 'aaa',
+          image: '',
+          path: '/path2',
+        ),
+      ];
+      final folderGames = [
+        Game(
+          name: 'game 1',
+          description: '',
+          image: '',
+          path: '/path1',
+        ),
+        Game(
+          name: 'game 2',
+          description: '',
+          image: '',
+          path: '/path2',
+        ),
+        Game(
+          name: 'game 3',
+          description: '',
+          image: '',
+          path: '/path3',
+        ),
+      ];
 
-    final games = syncGames(currentGames, folderGames);
-    expect(games.length, 3);
-    expect(games[0].description, 'aaa');
-    expect(games[1].description, 'aaa');
-  });
+      final games = syncGames(currentGames, folderGames);
+      expect(games.length, 3);
+      expect(games[0].description, 'aaa');
+      expect(games[1].description, 'aaa');
+    });
 
-  test('Sync games remove', () {
-    final currentGames = [
-      Game(
-        name: 'game 1',
-        description: 'aaa',
-        image: '',
-        path: '/path1',
-      ),
-      Game(
-        name: 'game 2',
-        description: 'aaa',
-        image: '',
-        path: '/path2',
-      ),
-    ];
-    final folderGames = [
-      Game(
-        name: 'game 1',
-        description: '',
-        image: '',
-        path: '/path1',
-      ),
-      Game(
-        name: 'game 3',
-        description: '',
-        image: '',
-        path: '/path3',
-      ),
-    ];
+    test('Sync games remove', () {
+      final currentGames = [
+        Game(
+          name: 'game 1',
+          description: 'aaa',
+          image: '',
+          path: '/path1',
+        ),
+        Game(
+          name: 'game 2',
+          description: 'aaa',
+          image: '',
+          path: '/path2',
+        ),
+      ];
+      final folderGames = [
+        Game(
+          name: 'game 1',
+          description: '',
+          image: '',
+          path: '/path1',
+        ),
+        Game(
+          name: 'game 3',
+          description: '',
+          image: '',
+          path: '/path3',
+        ),
+      ];
 
-    final games = syncGames(currentGames, folderGames);
-    expect(games.length, 2);
-    expect(games[0].description, 'aaa');
-    expect(games[1].description, '');
-    expect(games[1].name, 'game 3');
+      final games = syncGames(currentGames, folderGames);
+      expect(games.length, 2);
+      expect(games[0].description, 'aaa');
+      expect(games[1].description, '');
+      expect(games[1].name, 'game 3');
+    });
+
+    test('Should create platform', () async {
+      final gameCategory = GameCategory(id: 'asd', name: 'categ', image: 'asd');
+      final platform = PlatformModel(
+          id: 1,
+          folder: '/0/',
+          lastUpdate: DateTime.now(),
+          category: gameCategory,
+          games: <Game>[]);
+      await createPlatform(platform);
+    });
   });
 }
